bfc1d74d66f240a658f466a1df9518531dd48aa6
Selich <selich.work@gmail.com>
2020-10-17 20:29:29 +0200

    First commit

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'handwritting.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'handwriting.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
